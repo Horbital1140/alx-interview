4deeeebb35b59b457656b6134804137e6857afff
Horbital1140 <adeemma1993@gmail.com>
2024-01-11 14:17:33 +0100

    alx interview 2

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+def canUnlockAll(boxes):
+    opened_boxes = {0}
+    key_queue = boxes[0]
+    while key_queue:
+        
+        key = key_queue.pop(0)
+
+
+        if key < len(boxes) and key not in opened_boxes:
+            opened_boxes.add(key)
+            key_queue.extend(boxes[key])
+    return len(opened_boxes) == len(boxes)
+
